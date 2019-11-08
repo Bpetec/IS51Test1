2492ed7ad752c4cc8a41bf42b6d0f47519bd3fce
bpetec <bpetec@yahoo.com>
2019-11-07 17:40:34 -0800

    Adding exam-end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,15 @@ import { FlexModalService } from './shared-components/flex-modal/flex-modal.serv
 })
 export class AppComponent implements OnInit {
   title = 'final';
-  aboutText: string;
+  aboutText = 'This application is designed by Brandon Peterson. (C) 2018';
   constructor(private flexModal: FlexModalService) {
 
   }
 
   ngOnInit() {
   }
-
+  open() {
+    this.flexModal.openDialog('about-modal');
+  }
 
 }
